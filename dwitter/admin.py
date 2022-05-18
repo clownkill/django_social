5bdde0c4fb1a5262103b303e8f8bd6ca6811c213
clownkill <alehnovich90@yandex.ru>
2022-05-18 17:11:02 +0300

    add profile model

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 
+from .models import Profile
+
 
 class UserAdmin(admin.ModelAdmin):
     model = User
@@ -9,4 +11,5 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
+admin.site.register(Profile)
 
