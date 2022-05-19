dc1c1129dba7147052d31f0e895ffb5190f7494a
clownkill <alehnovich90@yandex.ru>
2022-05-19 14:27:58 +0300

    add post signal

--- dwitter/models.py
+++ dwitter/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 
 
@@ -15,3 +17,14 @@ class Profile(models.Model):
         blank=True
     )
 
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.add(instance.profile)
+        user_profile.save()
