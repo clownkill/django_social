699f293d9bcf4ba75daa322bf0a500271f789d25
clownkill <alehnovich90@yandex.ru>
2022-05-20 17:28:20 +0300

    add profile + follow/unfollow buttons

--- dwitter/urls.py
+++ dwitter/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import dashboard, profile_list
+from .views import dashboard, profile_list, profile
 
 
 app_name = 'dwitter'
@@ -8,4 +8,5 @@ app_name = 'dwitter'
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('profile_list/', profile_list, name='profile_list'),
+    path('profile/<int:pk>', profile, name='profile'),
 ]
